0fe108e5b5321a8ff3359b80cb9e9e6f631ca2d7
trvny <120686325+trvny@users.noreply.github.com>
2026-09-22 14:44:06 +0200

    test: migrate Compose Android rule to v2

--- app/src/androidTest/java/ais/tee/WebProviderSignInTest.kt
+++ app/src/androidTest/java/ais/tee/WebProviderSignInTest.kt
@@ -2,7 +2,7 @@ package ais.tee
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsSelected
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
